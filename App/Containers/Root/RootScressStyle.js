ac513b761db359a8236533f06c581214ab4870a7
Przemysław Prencel <hartag96@gmail.com>
2019-10-20 17:17:24 +0200

    build project structure

---
+++ App/Containers/Root/RootScressStyle.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textMargin: {
+    marginVertical: 5
+  },
+  frontButton: {
+    width: 100
+  },
+  mailInput: {
+    borderWidth: 1,
+    marginVertical: 10,
+    padding: 10,
+    width: '60%',
+    borderColor: '#222222',
+  },
+  passwordInput: {
+    borderWidth: 1,
+    marginVertical: 13,
+    padding: 10,
+    width: '60%',
+    borderColor: '#222222',
+  }
+});
