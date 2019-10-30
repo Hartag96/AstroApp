ad3a2ecde4973f6f4b053aa1518e3d0938bf88f3
Piotr Zając <p.zajac@samorzad.p.lodz.pl>
2019-10-30 18:58:44 +0100

    Add all input

--- App/Containers/Registration/RegistrationScreen.js
+++ App/Containers/Registration/RegistrationScreen.js
@@ -10,11 +10,12 @@ export default class RegistrationScreen extends Component {
                     style={{width: 200, height: 100}}
                     source={{uri: 'https://res-3.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1415705305/gbokfpaf2lup8qyz2bva.png'}}
                 />
-                <TextInput style={styles.mailInput} placeholder="jan@dzban.com"></TextInput>
-                <TextInput style={styles.passwordInput} placeholder="password" secureTextEntry={true}></TextInput>
-                <Button title="Log in" style={styles.frontButton}></Button>
-                <Text style={styles.textMargin}>Or</Text>
-                <Button title="Sign up" style={styles.frontButton}></Button>
+                <TextInput style={styles.mailInput} placeholder="First name"/>
+                <TextInput style={styles.mailInput} placeholder="Last name"/>
+                <TextInput style={styles.mailInput} placeholder="jan@dzban.com"/>
+                <TextInput style={styles.passwordInput} placeholder="password" secureTextEntry={true}/>
+                <TextInput style={styles.passwordInput} placeholder="Confirm password" secureTextEntry={true}/>
+                <Button title="Sign up" style={styles.frontButton}/>
             </View>
         )
     }
