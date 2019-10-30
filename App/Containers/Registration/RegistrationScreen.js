635a96f288e7453374ae4da4b39171d2d283ea29
Piotr Zając <p.zajac@samorzad.p.lodz.pl>
2019-10-30 20:56:47 +0100

    Send value from TextInput to database (without password)

--- App/Containers/Registration/RegistrationScreen.js
+++ App/Containers/Registration/RegistrationScreen.js
@@ -1,8 +1,54 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, Image } from 'react-native';
+import { StyleSheet, Alert, Permissions, Text, View, Button, TextInput, Image } from 'react-native';
 import styles from './RegistrationStyle';
 
 export default class RegistrationScreen extends Component {
+    constructor() {
+        super();
+        this.state = {
+            view: 0
+        };
+    }
+    componentMountDid() {
+
+    }
+
+    state = {
+        firstname: 'demo',
+        lastname: 'demo',
+        mail: 'demo',
+        password: 'demo',
+        confirmpassword: 'demo'
+    };
+
+    handlePress = async () => {
+        try {
+            const astroApiCall = await fetch('https://astro-api-dev.herokuapp.com/users/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    "user": {
+                        "email": this.state.mail,
+                        "password": "1qdadaaz@WSX",
+                        "password_confirmation": "1qdadaaz@WSX",
+                        "first_name": this.state.firstname,
+                        "last_name": this.state.lastname
+                    }
+                })
+            });
+
+            const astro = await astroApiCall.text();
+            console.log(astro);
+            Alert.alert(astro);
+        } catch(err) {
+            console.error(err);
+            Alert.alert("Lolek");
+        }
+    }
+
+
     render() {
         return (
             <View style={styles.container}>
@@ -10,12 +56,12 @@ export default class RegistrationScreen extends Component {
                     style={{width: 200, height: 100}}
                     source={{uri: 'https://res-3.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1415705305/gbokfpaf2lup8qyz2bva.png'}}
                 />
-                <TextInput style={styles.mailInput} placeholder="First name"/>
-                <TextInput style={styles.mailInput} placeholder="Last name"/>
-                <TextInput style={styles.mailInput} placeholder="jan@dzban.com"/>
-                <TextInput style={styles.passwordInput} placeholder="Password" secureTextEntry={true}/>
-                <TextInput style={styles.passwordInput} placeholder="Confirm password" secureTextEntry={true}/>
-                <Button title="Sign up" style={styles.frontButton}/>
+                <TextInput style={styles.mailInput} onChangeText={(value) => this.setState({firstname: value})} placeholder="First name"/>
+                <TextInput style={styles.mailInput} onChangeText={(value) => this.setState({lastname: value})} placeholder="Last name"/>
+                <TextInput style={styles.mailInput} onChangeText={(value) => this.setState({mail: value})} placeholder="jan@dzban.com"/>
+                <TextInput style={styles.passwordInput} onChangeText={(value) => this.setState({password: value})} placeholder="Password" secureTextEntry={true}/>
+                <TextInput style={styles.passwordInput} onChangeText={(value) => this.setState({confirmpassword: value})} placeholder="Confirm password" secureTextEntry={true}/>
+                <Button title="Sign up" style={styles.frontButton} onPress={this.handlePress.bind(this)} />
             </View>
         )
     }
