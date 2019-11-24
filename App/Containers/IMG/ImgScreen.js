e4441692e7624e2e1862b77558be77b5d4cd829d
Piotr Zając <p.zajac@samorzad.p.lodz.pl>
2019-11-24 23:56:51 +0100

    Send image (base64) to API Imgur and get URL

--- App/Containers/IMG/ImgScreen.js
+++ App/Containers/IMG/ImgScreen.js
@@ -14,8 +14,48 @@ export default class RootScreen extends Component {
       hasCameraPermission: null,
       image: null,
       base: null,
+      isModalVisible: false,
+      modalText: 'Success :o',
+      modalImage: 'https://png.pngtree.com/svg/20170724/success_405070.png',
+      modalButtonText: 'Close'
     }
   }
+
+  static navigationOptions = ({ navigate, navigation }) => ({
+    title: "Share Photo",
+    headerRight: <Button style={{marginRight: 10}} title="Logout" onPress={ async ()=>{ await AsyncStorage.setItem("auth_token", '').then(() => {
+      navigation.navigate('Login');
+    }); }} />,
+  })
+
+  hideModal = () => {
+    this.setState({isModalVisible: false});
+}
+
+showModal = (type) => {
+    var text = '';
+    var image = 'https://png.pngtree.com/svg/20170818/fail_641019.png';
+    var btnText = 'Try again';
+
+    if(type == 'success'){
+        text = 'We have url to image :D!';
+        image = 'https://png.pngtree.com/svg/20170724/success_405070.png';
+        btnText = 'Close';
+    }else if(type == 'error'){
+        text = 'Error'
+    }else{
+        text = 'Something went wrong'
+    }
+
+    this.setState({ 
+        isModalVisible: true, 
+        modalText: text, 
+        modalImage: image,
+        modalButtonText: btnText 
+    });
+};
+
+
   async componentDidMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL); // CAMERA_ROLL ->	READ_EXTERNAL_STORAGE, WRITE_EXTERNAL_STORAGE
     this.setState({ hasCameraPermission: status === "granted" });
@@ -30,6 +70,33 @@ export default class RootScreen extends Component {
     if (!result.cancelled) {
       this.setState({ image: result.uri, base: result.base64 });
     }
+
+    const formData = new FormData();
+    formData.append("type", 'base64')
+    formData.append("image", result.base64)
+
+    try {
+      const astroApiCall = await fetch('https://api.imgur.com/3/upload/', {
+          method: 'POST',
+          headers: {
+            "Content-Type": 'multipart/form-data',
+            "Authorization": "Client-ID f05a30dbc8032e3",
+          },
+          body: formData
+      });
+
+      const astro = await astroApiCall.json();
+    //  console.log("Test 13", astro);
+      if(astro.success) {
+          this.showModal('success');
+          console.log("Testowy test", astro.data.link);
+      } else {
+          this.showModal('error');
+      }
+  } catch (err) {
+      this.showModal('error');
+      console.log("Err");
+  }
   }
 
   render() {
@@ -51,6 +118,16 @@ export default class RootScreen extends Component {
         <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
           <Button onPress={this._getPhotoLibrary.bind(this)} title="Share Photo!"/>
         </View>
+        <Modal isVisible={this.state.isModalVisible}>
+            <View style={styles.modal}>
+                <Image
+                style={{width: 100, height:100}}
+                source={{uri: this.state.modalImage}}
+                />
+                <Text style={{paddingBottom: 15, paddingTop: 15}}>{this.state.modalText}</Text>
+                <Button stype={{flex: 1}} title={this.state.modalButtonText} onPress={this.hideModal} />
+            </View>
+        </Modal>
       </View>
      );
     }
