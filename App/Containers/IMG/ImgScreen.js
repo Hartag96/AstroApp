7004b7ac302cc1beb006820afab71da47e579ab1
Piotr Zając <p.zajac@samorzad.p.lodz.pl>
2019-11-24 18:20:36 +0100

    clean

--- App/Containers/IMG/ImgScreen.js
+++ App/Containers/IMG/ImgScreen.js
@@ -4,14 +4,6 @@ import Modal from "react-native-modal";
 import {AsyncStorage} from 'react-native';
 import styles from './ImgScreenStyle';
 import { withOrientation } from 'react-navigation';
-import ImagePicker from 'react-native-image-picker';
-import { TouchableOpacity } from 'react-native-gesture-handler';
-
-const options = {
-  title: 'Choose image to share',
-  chooseFromLibraryButtonTitle: 'Choose image from library',
-  takePhotoButtonTitle: 'Take photo with camera',
-}
 
 export default class RootScreen extends Component {
   state = {
@@ -21,13 +13,6 @@ export default class RootScreen extends Component {
     modalButtonText: 'Try again'
   };
 
-  myfun=()=>{
-    alert('clicked');
-  }
-  static navigationOptions = ({ navigate, navigation }) => ({
-    title: "IMG"
-  })
-
   render() {
     return (
       <View style={styles.container}>
@@ -36,10 +21,6 @@ export default class RootScreen extends Component {
             style={{width: 200, height: 100}}
             source={{uri: 'https://res-3.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco/v1415705305/gbokfpaf2lup8qyz2bva.png'}}
           />
-
-        <TouchableOpacity style={{backgroundColor:'green',margin:10,padding:10}} onPress={this.myfun}>
-          <Text style={{color:'#fff'}}>Select Image</Text>
-        </TouchableOpacity>
 
         </View>
       </View>
